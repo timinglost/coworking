a859124bc48afe444c15d48aa0d6bb5b73789c61
Konstantin <konstantinkail349@gmail.com>
2022-06-27 02:01:49 +0300

    fix import error

--- coworking/detailsapp/urls.py
+++ coworking/detailsapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from coworking.detailsapp.views import show_details
+from detailsapp.views import show_details
 
 urlpatterns = [
     path('', show_details, name='details')
