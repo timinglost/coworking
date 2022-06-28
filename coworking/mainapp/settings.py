6144b1efb727f6a2856103486f41a7e407f0692e
timinglost <76259476+timinglost@users.noreply.github.com>
2022-06-28 16:52:01 +0300

    Update settings.py

--- coworking/mainapp/settings.py
+++ coworking/mainapp/settings.py
@@ -145,7 +145,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #
 
 AUTH_USER_MODEL = 'authapp.UserModel'
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
