f5099ef8fa5517433a035641072a3dd496c97f55
Kirill <budanovkirilldmitrievich@gmail.com>
2022-06-29 23:52:42 +0300

    feedbackapp done

--- coworking/feedbackapp/admin.py
+++ coworking/feedbackapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from feedbackapp.models import Contact, QuestionCategory, Question, Message
 
-# Register your models here.
+admin.site.register(Contact)
+admin.site.register(QuestionCategory)
+admin.site.register(Question)
+admin.site.register(Message)
