4d709086eac1a91c520c7057691c6752378db843
Senishine <oksastar89@mail.ru>
2022-07-02 16:05:51 +0300

    added address validation on ui side

--- coworking/mainapp/settings.py
+++ coworking/mainapp/settings.py
@@ -143,7 +143,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #
-
-AUTH_USER_MODEL = 'authapp.UserModel'
-
-PHONENUMBER_DEFAULT_REGION = 'RU'
