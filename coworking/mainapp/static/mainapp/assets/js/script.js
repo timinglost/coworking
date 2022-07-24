f66bad8c6260a862a5a2d8e3a05d3811882f45cf
DenSarRu <fIght-2003>
2022-07-24 19:22:14 +0300

    sprint #5. mainapp. search row update

--- coworking/mainapp/static/mainapp/assets/js/script.js
+++ coworking/mainapp/static/mainapp/assets/js/script.js
@@ -1,99 +1,13 @@
+var myMap;
+
 ymaps.ready(init);
-
-
-let check_was_done = false;
-
-
-
-function onNewAd(event){
-    if(check_was_done) {
-        check_was_done = false;
-        return;
-    }
-    event.preventDefault();
-    const address = $('#address').val();
-
-    if(!address){
-        showError('Address must not be empty');
-        return;
-    }
-
-    checkAddress(address)
-        .then(result => {
-            if(result.error){
-                showError(result.error);
-                return;
-            }
-            check_was_done = true;
-            $(event.srcElement).trigger('click');
-        }, err => {
-            showError('Internal server error');
-        })
-}
-
-function onFileUpload(event){
-    const uploadedFiles = $('#uploaded-files');
-    uploadedFiles.empty();
-    const fileList = event.target.files;
-    for(let i=0; i < fileList.length; i++) {
-        const { name: fileName, size } = fileList[i];
-        const fileSize = (size / 1000).toFixed(2);
-
-        const src = URL.createObjectURL(fileList[i]);
-
-        const img = $('<img class="preview-img"></img>');
-        img.attr('src', src);
-
-        const container = $('<div class="preview-img-container"/>');
-        const description = $('<span/>');
-        description.text(`${fileName} - ${fileSize}KB`);
-        container.append(img);
-        container.append(description);
-
-        uploadedFiles.append(container);
-    }
-}
-
-function onAmenitySelected(event){
-    let div = $(event.target);
-    while(!div.hasClass('amenity-clickable')) {
-        div = div.parent();
-    }
-
-    const convid = div.attr('convid');
-    if(!convid) {
-        return;
-    }
-    const prevValue = $('#id_selected_amenities').val();
-    const currentAmenities = prevValue ? prevValue.split(',') : [];
-    if (div.attr('selected')) {
-        div.removeAttr('selected');
-        div.children().css({'color': 'var(--black_40)', 'font-weight': 'normal'});
-        const index = currentAmenities.indexOf(convid);
-        if (index > -1) { // only splice array when item is found
-          currentAmenities.splice(index, 1); // 2nd parameter means remove one item only
-        }
-    }else {
-        div.attr('selected', true);
-        div.children().css({'color': 'black', 'font-weight': 'bolder'});
-        currentAmenities.push(convid);
-    }
-    $('#id_selected_amenities').val(currentAmenities.join(','));
-}
-
-function showError(message) {
-    $('#address-notice').text(message);
-    $('#address').addClass('input_error');
-    $('#address-notice').css('display', 'block');
-}
-
-function clearAddressErrors(){
-    $('#address').removeClass('input_error');
-    $('#address-notice').css('display', 'none');
-}
 
 function init() {
 
+  var address = new ymaps.SuggestView(
+    'suggest',
+    {provider: provider}
+  );
 
     $(document).ready(function(){
         $('#id_image').attr("multiple","true");
@@ -205,7 +119,98 @@ function init() {
         return myPlacemark;
     }
 
+};
+
+
+let check_was_done = false;
+
+function onNewAd(event){
+    if(check_was_done) {
+        check_was_done = false;
+        return;
+    }
+    event.preventDefault();
+    const address = $('#address').val();
+
+    if(!address){
+        showError('Address must not be empty');
+        return;
+    }
+
+    checkAddress(address)
+        .then(result => {
+            if(result.error){
+                showError(result.error);
+                return;
+            }
+            check_was_done = true;
+            $(event.srcElement).trigger('click');
+        }, err => {
+            showError('Internal server error');
+        })
 }
+
+function onFileUpload(event){
+    const uploadedFiles = $('#uploaded-files');
+    uploadedFiles.empty();
+    const fileList = event.target.files;
+    for(let i=0; i < fileList.length; i++) {
+        const { name: fileName, size } = fileList[i];
+        const fileSize = (size / 1000).toFixed(2);
+
+        const src = URL.createObjectURL(fileList[i]);
+
+        const img = $('<img class="preview-img"></img>');
+        img.attr('src', src);
+
+        const container = $('<div class="preview-img-container"/>');
+        const description = $('<span/>');
+        description.text(`${fileName} - ${fileSize}KB`);
+        container.append(img);
+        container.append(description);
+
+        uploadedFiles.append(container);
+    }
+}
+
+function onAmenitySelected(event){
+    let div = $(event.target);
+    while(!div.hasClass('amenity-clickable')) {
+        div = div.parent();
+    }
+
+    const convid = div.attr('convid');
+    if(!convid) {
+        return;
+    }
+    const prevValue = $('#id_selected_amenities').val();
+    const currentAmenities = prevValue ? prevValue.split(',') : [];
+    if (div.attr('selected')) {
+        div.removeAttr('selected');
+        div.children().css({'color': 'var(--black_40)', 'font-weight': 'normal'});
+        const index = currentAmenities.indexOf(convid);
+        if (index > -1) { // only splice array when item is found
+          currentAmenities.splice(index, 1); // 2nd parameter means remove one item only
+        }
+    }else {
+        div.attr('selected', true);
+        div.children().css({'color': 'black', 'font-weight': 'bolder'});
+        currentAmenities.push(convid);
+    }
+    $('#id_selected_amenities').val(currentAmenities.join(','));
+}
+
+function showError(message) {
+    $('#address-notice').text(message);
+    $('#address').addClass('input_error');
+    $('#address-notice').css('display', 'block');
+}
+
+function clearAddressErrors(){
+    $('#address').removeClass('input_error');
+    $('#address-notice').css('display', 'none');
+}
+
 
 function checkAddress(address) {
     return ymaps.geocode(address).then(function (res) {
@@ -245,3 +250,22 @@ function checkAddress(address) {
             };
         });
 }
+
+
+var provider = {
+  suggest :  function(request, options) {
+    var resultArray = [];
+    var suggest = new ymaps.suggest(request);
+    var result = suggest.then( items => {
+      for (const i of items) {
+        resultArray.push({
+          value: i.value.split(',').pop().replace(/\s+/g, ''),
+          displayName: i.displayName.split(',').slice(0,2)
+        });
+      }
+
+      return ymaps.vow.resolve(resultArray);
+    });
+        return ymaps.vow.resolve(result);
+  }
+};
