c9264fceae748aa46877caa4b2525cdaa9c0cf5b
Senishine <oksastar89@mail.ru>
2022-07-25 02:51:37 +0300

    main search fix

--- coworking/mainapp/views.py
+++ coworking/mainapp/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
-from createapp.models import Room
+from createapp.models import Room, OfferImages
 from mainapp.forms import SearchMainForm
 
 
 def main(request):
     title = 'ЛОКАЦИЯ | Сервис для поиска коворкинга в России'
     form = SearchMainForm()
+    rooms = list(Room.objects.all()[:3])
+    offer_images = OfferImages.objects.filter(room__in=rooms)
     context = {
         'title': title,
         'form': form,
-        'rooms': Room.objects.all()[:3]
+        'offer_images': offer_images
     }
     return render(request, 'mainapp/index.html', context)
