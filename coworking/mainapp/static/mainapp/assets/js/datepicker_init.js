4c8bb3751bcda6200aa8ce495382e19deaabba22
Senishine <oksastar89@mail.ru>
2022-07-28 19:00:32 +0300

    fix datepicker script

--- coworking/mainapp/static/mainapp/assets/js/datepicker_init.js
+++ coworking/mainapp/static/mainapp/assets/js/datepicker_init.js
@@ -5,27 +5,32 @@ function initDatePicker(config){
     const fromId = config.fromId ?? 'date-from';
     const toId = config.toId ?? 'date-to';
 
-    $(document).ready(() => {
+    return new Promise((resolve) => {
+        $(document).ready(() => {
 
-        const initialStart = moment().startOf('day');
-        const initialEnd = moment().startOf('day').add(8, 'day');
+            const initialStart = moment().startOf('day');
+            const initialEnd = moment().startOf('day').add(8, 'day');
 
-        const onDateSelected = (start, end, label) => {
-            $('#' + fromId).val(start.format(format));
-            $('#' + toId).val(end.format(format));
-        };
+            const onDateSelected = (start, end, label) => {
+                $('#' + fromId).val(start.format(format));
+                $('#' + toId).val(end.format(format));
+            };
 
-        onDateSelected(initialStart, initialEnd);
+            onDateSelected(initialStart, initialEnd);
+            const datePicker = $('#' + inputId);
+            datePicker.daterangepicker({
+                opens: 'left',
+                minDate: moment().startOf('day'),
+                startDate: initialStart,
+                endDate: initialEnd,
+                 locale: {
+                  format: format
+                }
+            }, onDateSelected);
 
-        $('#' + inputId).daterangepicker({
-            opens: 'left',
-            minDate: moment().startOf('day'),
-            startDate: initialStart,
-            endDate: initialEnd,
-             locale: {
-              format: format
-            }
-        }, onDateSelected);
+            return resolve(datePicker.data('daterangepicker'));
+        });
     });
+
 }
 
