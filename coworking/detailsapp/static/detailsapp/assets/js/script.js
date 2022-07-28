58d4e8cb6f004c5c4c824fffb40f83f28559cd1d
Konstantin <konstantinkail349@gmail.com>
2022-07-28 22:16:52 +0300

    Revert "fix datepicker script"
    This reverts commit d00000c79cbf7478c7e37f54d1222338e318faff.

--- coworking/detailsapp/static/detailsapp/assets/js/script.js
+++ coworking/detailsapp/static/detailsapp/assets/js/script.js
@@ -1,58 +1,58 @@
 window.onload = function () {
-    const datePickerId = '#date-range';
+    var seats = document.querySelector('.select-seats')
+    var date_form = document.querySelector('input[id=date-range]')
+    var periods = document.querySelector('.form-peoples')
+    var confirm_btn = document.querySelector('.date-btn')
+    var apply_btn = document.querySelector('.applyBtn')
+    var start_date = document.querySelector('input[id=date-from]')
+    var end_date = document.querySelector('input[id=date-to]')
 
-    var seats = document.querySelector('.select-seats');
-    var periods = document.querySelector('.form-peoples');
-    var confirm_btn = document.querySelector('.date-btn');
-    var apply_btn = document.querySelector('.applyBtn');
-    var start_date = document.querySelector('input[id=date-from]');
-    var end_date = document.querySelector('input[id=date-to]');
+    date_form.addEventListener('change', function (event) {
+            console.log('change')
+            console.log(date_form.value)
+            RenderSeats(start_date.value, end_date.value, seats.value);
+        });
+    seats.addEventListener('change', function (event) {
+            RenderSeats(start_date.value, end_date.value, seats.value);
+    });
 
-    const dataProvider = () => $(datePickerId).data('daterangepicker');
-    $(datePickerId).on(
-        'apply.daterangepicker',
-        (ev, picker) => {
-            startDate = picker.startDate.format('YYYY-MM-DD');
-            endDate = picker.endDate.format('YYYY-MM-DD');
-            renderSeats(startDate, endDate, seats.value, dataProvider);
-        }
-    );
-    seats.addEventListener(
-        'change',
-        (event) => renderSeats(start_date.value, end_date.value, seats.value, dataProvider)
-    );
 
-    function renderSeats(start_date, end_date, seats, dataProvider) {
+    function RenderSeats(start_date, end_date, seats) {
         console.log(start_date, end_date, seats)
-        if(!start_date || !end_date || !seats) {
-            return;
-        }
-        periods.classList.remove('hidden');
-        $.ajax({
+        if(start_date && end_date && seats) {
+
+            periods.classList.remove('hidden');
+            $.ajax({
             url: window.location.pathname + start_date + "/" + end_date + "/" + seats + "/",
-            success: (data) => {
+
+            success: function (data) {
                 $('.form-peoples').html(data.result);
-                $(document).ready(() => {
-                    $('#select_period').on('change', (event) => {
-                        var dates = event.target.selectedOptions[0].innerText;
-                        if (!isNaN(dates)) {
-                            return;
-                        }
+                $(document).ready(function() {
+                $('select.period-select').on('change', function(event){
+                    var dates = event.target.selectedOptions[0].innerText;
+                    if (isNaN(dates)) {
                         var selected_dates = dates.matchAll(/\d{2}\/\d{2}\/\d\d\d\d/g);
                         selected_dates = Array.from(selected_dates)
                         var date_from = selected_dates[0][0].split('/').reverse().join('-');
                         var date_to = selected_dates[1][0].split('/').reverse().join('-');
-                        const datePickerData = dataProvider();
-                        datePickerData.setStartDate(date_from);
-                        datePickerData.setEndDate(date_to);
+                        document.querySelector('input[id=date-from]').value = date_from;
+                        document.querySelector('input[id=date-to]').value = date_to;
+                        document.querySelector('input[id=date-range]').value = date_from + ' - ' + date_to;
                         document.querySelector('.date-btn').classList.remove('hidden');
-//                        $.ajax({
-//                            url: window.location.pathname + document.querySelector('input[id=date-from]').value + "/" + document.querySelector('input[id=date-to]').value + "/" + seats + "/",
-//                            success: (data) => $('.form-peoples').html(data.result)
-//                        })
-                    });
+                        $.ajax({
+                        url: window.location.pathname + document.querySelector('input[id=date-from]').value + "/" + document.querySelector('input[id=date-to]').value + "/" + seats + "/",
+
+                        success: function (data) {
+                            $('.form-peoples').html(data.result);
+                            }
+                        })
+                    }
+
                 });
+            });
             },
         });
+        }
     }
+
 }
