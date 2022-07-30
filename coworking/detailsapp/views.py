34d2be9c29003ad5fcb7181d4d4f956879159226
Konstantin <konstantinkail349@gmail.com>
2022-07-30 12:17:51 +0300

    sprint 6: update view

--- coworking/detailsapp/views.py
+++ coworking/detailsapp/views.py
@@ -277,7 +277,7 @@ def get_available_seats(request, pk, start_date, end_date, seats):
                 return JsonResponse({'result': result})
         except:
             context = {
-                'available_seats': [_ for _ in range(1, offer.seats_number + 1)]
+                'available_seats': [_ for _ in range(1, seats + 1)]
             }
             result = render_to_string('detailsapp/includes/inc_seats.html', context)
             return JsonResponse({'result': result})
