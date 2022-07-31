22b37f7b3374be231f95f605d1ba0234520d2d35
Konstantin <konstantinkail349@gmail.com>
2022-07-31 17:30:01 +0300

    sprint 6: update view

--- coworking/mainapp/views.py
+++ coworking/mainapp/views.py
@@ -13,8 +13,8 @@ from userapp.models import UserModel
 
 def get_top_landlords():
     all_landlords = {
-        user: f"{numpy.average([OffersRatings.objects.get(offer=_).summary_rating for _ in Room.objects.filter(is_active=True, is_published=False, room_owner=user)]):.1f}"
-        for user in UserModel.objects.filter(is_landlord=True)}
+        user: f"{numpy.average([OffersRatings.objects.get(offer=_).summary_rating for _ in Room.objects.filter(room_owner=user, is_active=True, is_published=True)]):.1f}"
+        for user in UserModel.objects.filter(is_landlord=True) if Room.objects.filter(room_owner=user, is_active=True, is_published=True).exists()}
     return [k for k, v in sorted(all_landlords.items(), key=lambda item: item[1])[:3]]
 
 
@@ -22,10 +22,10 @@ def avg_rating_per_criteria(landlord, criteria_name):
     room_ratings = [
         Evaluations.objects.filter(offer=room).filter(rating_name=criteria_name).aggregate(
             rating=Avg("evaluation"))['rating']
-        for room in Room.objects.filter(is_active=True, is_published=False, room_owner=landlord)
+        for room in Room.objects.filter( room_owner=landlord, is_active=True, is_published=True)
     ]
-    room_ratings = list(map(lambda it: int(it), filter(lambda it: math.isnan(it), room_ratings)))
-    return int(numpy.average() * 10) if len(room_ratings) > 0 else 0
+    room_ratings = list(map(lambda it: int(it), room_ratings))
+    return int(numpy.average(room_ratings) * 10) if len(room_ratings) > 0 else 0
 
 
 def main(request):
@@ -38,15 +38,16 @@ def main(request):
     else:
         form = SearchMainForm()
     offer_ratings = list(OffersRatings.objects.order_by('-summary_rating')[:3])
-    rooms = list(map(lambda x: x.offer, offer_ratings))
+    rooms = list(map(lambda x: x.offer if x.offer.is_active and x.offer.is_published else None, offer_ratings))
     offer_images = OfferImages.objects.filter(room__in=rooms)
 
     room_data = {}
 
-    for rating in offer_ratings:
-        room_data[rating.offer] = {
-            'rating': rating.summary_rating
-        }
+    for room in rooms:
+        if room:
+            room_data[room] = {
+                'rating': OffersRatings.objects.get(offer=room).summary_rating
+            }
     rating_names = RatingNames.objects.all()
     top_landlords = {
         landlord: {
